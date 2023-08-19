55970aada954b31c783c8a4e16d23c660ae05002
Karell Peter <53671374+petervillaroman@users.noreply.github.com>
2023-08-19 01:07:37 -0700

    implemented progress bar

--- index.py
+++ index.py
@@ -1,16 +1,27 @@
 from pytube import YouTube
 from sys import argv
+from tqdm import tqdm
 
-link = argv[1]
-yt = YouTube(link)
+class TqdmUpTo(tqdm):
+    def update_to(self, stream, chunk, bytes_remaining):
+        self.update(len(chunk))  # update the size of the chunk downloaded
 
-print("Title: ", yt.title)
-print("Number of views: ", yt.views)
+def main():
+    link = argv[1]
+    yt = YouTube(link)
 
-# To get the length of video
-print("Length of video: ", yt.length, "seconds")
+    print("Title: ", yt.title)
+    print("Number of views: ", yt.views)
+    print("Length of video: ", yt.length, "seconds")
 
+    yd = yt.streams.get_highest_resolution()
+    file_size = yd.filesize
 
-yd = yt.streams.get_highest_resolution()
-yd.download('/Users/Peter/downloadedVideos')
-print("Download completed!!")
+    with TqdmUpTo(total=file_size, unit='B', unit_scale=True, desc="Download In Progress", ascii=True) as t:
+        yt.register_on_progress_callback(t.update_to)
+        yd.download('/Users/Peter/downloadedVideos')
+
+    print("Download completed!!")
+
+if __name__ == '__main__':
+    main()
