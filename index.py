eadea17a2ae72811c5a03aee2310c6636d79f3e2
Karell Peter <53671374+petervillaroman@users.noreply.github.com>
2023-08-20 04:21:09 -0700

    added an audio option.
    TODO: it downloads as webm sometimes... need to find a way to ensure mp3/mp4 format.

--- index.py
+++ index.py
@@ -2,37 +2,59 @@ from pytube import YouTube
 from sys import argv
 from rich.progress import Progress
 from rich.console import Console
+import questionary
 
 console = Console()
 progress = Progress(console=console)
 
-# Create a task outside of the callback to avoid creating it multiple times.
+# Create a task outside the callback to avoid creating it multiple times.
 task = progress.add_task("[cyan]Downloading...", total=0)  # Initialize with total=0, we'll update it later.
+
 
 def rich_progress_callback(stream, chunk, bytes_remaining):
     # Update the progress
     progress.update(task, advance=len(chunk))
 
+
 def main():
-    link = argv[1]
+    link = input("Enter the link of YouTube video you want to download:  ")
     yt = YouTube(link)
 
-    print("Title: ", yt.title)
-    print("Number of views: ", yt.views)
-    print("Length of video: ", yt.length, "seconds")
+    # prompt user if he wants to download the video or just the audio
+    download_type = questionary.select(
+        "Do you want to download the video or just the audio?",
+        choices=['Video', 'Audio'],
+    ).ask()
 
-    yd = yt.streams.get_highest_resolution()
-    file_size = yd.filesize
+    if download_type == "Video":
+        print("Title: ", yt.title)
+        print("Number of views: ", yt.views)
+        print("Length of video: ", yt.length, "seconds")
 
-    # Update the total filesize of the task now that we have it.
-    progress.update(task, total=file_size)
+        print("Downloading video...")
+        yd = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first()
+        file_size = yd.filesize
+        progress.update(task, total=file_size)
+        yt.register_on_progress_callback(rich_progress_callback)
+        with progress:
+            yd.download('/Users/Peter/ytDownloads/downloadedVideos')
+    else:
+        print("Title: ", yt.title)
+        print("Number of views: ", yt.views)
+        print("Length of video: ", yt.length, "seconds")
 
-    # Register the callback and start the download.
-    yt.register_on_progress_callback(rich_progress_callback)
-    with progress:
-        yd.download('/Users/Peter/downloadedVideos')
+        print("Downloading audio...")
+        yd = yt.streams.filter(only_audio=True).order_by('abr').desc().first()
+        file_size = yd.filesize
+        progress.update(task, total=file_size)
+        yt.register_on_progress_callback(rich_progress_callback)
+        with progress:
+            yd.download('/Users/Peter/ytDownloads/downloadedAudios')
 
-    print("Download completed!!")
+
+
+
+
 
 if __name__ == '__main__':
     main()
